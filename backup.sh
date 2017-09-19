c00a3f4fdcee9b19f81e85ab5fc39f32ec58b2a6
andreas <andreas_osby@hotmail.com>
2017-09-19 21:05:49 +0200

    backup server

---
+++ backup.sh
@@ -0,0 +1,12 @@
+# backup packages
+dpkg --get-selections | grep -v deinstall > /media/backup/backup_packages
+sudo cp /media/backup/backup_packages /media/discworld/backup/
+# install with:
+
+# sudo dpkg --set-selections <./backup_packages
+# apt-get -y update
+# apt-get dselect-upgrade
+
+# backup files
+sudo tar czf /media/backup/backup.tar.gz --exclude=/backup_packages --exclude=/backup.tar.gz --exclude=/bin --exclude=/home/desktop --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc  --exclude=/sys --exclude=/tmp /
+sudo cp /media/backup/backup.tar.gz /media/discworld/backup
