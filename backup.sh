1d96859dcce506162b9595ce6281187d7712229a
andreas <andreas_osby@hotmail.com>
2020-04-19 20:45:17 +0200

    update backup script for skorstten

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 # backup packages
 dpkg --get-selections | grep -v deinstall > /media/backup/backup_packages
-sudo cp /media/backup/backup_packages /media/discworld/backup/
+# sudo cp /media/backup/backup_packages /media/discworld/backup/
+
 # install with:
 
 # sudo dpkg --set-selections <./backup_packages
@@ -8,5 +9,7 @@ sudo cp /media/backup/backup_packages /media/discworld/backup/
 # apt-get dselect-upgrade
 
 # backup files
-sudo tar czf /media/backup/backup.tar.gz --exclude=/backup_packages --exclude=/backup.tar.gz --exclude=/bin --exclude=/home/desktop --exclude=/var/lib/docker/overlay2 --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc  --exclude=/sys --exclude=/tmp /
-sudo cp /media/backup/backup.tar.gz /media/discworld/backup
+# join split files "cat os_backup.part* > backup.tar.gz.joined"
+# restore "tar xvpfz backup.tar.gz.joined -C /"
+sudo tar -czf - --exclude=backup_packages --exclude=backup.tar.gz --exclude=/bin --exclude=/home/desktop --exclude=/var/lib/docker/overlay2 --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc  --exclude=/sys --exclude=/tmp / | split -b 256m - "/media/backup/os_backup-part"
+sudo cp /media/backup/* /media/discworld/backup
