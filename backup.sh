294ee3d50cc12ca2a3e091d6ce2732eec62794d7
andreas <andreas_osby@hotmail.com>
2020-04-26 20:41:58 +0200

    reset size of bachup files to 512mb

--- backup.sh
+++ backup.sh
@@ -11,5 +11,5 @@ dpkg --get-selections | grep -v deinstall > /media/backup/backup_packages
 # backup files
 # join split files "cat os_backup.part* > backup.tar.gz.joined"
 # restore "tar xvpfz backup.tar.gz.joined -C /"
-sudo tar -czf - --exclude=backup_packages --exclude=backup.tar.gz --exclude=/bin --exclude=/home/desktop --exclude=/var/lib/docker/overlay2 --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc  --exclude=/sys --exclude=/tmp / | split -b 256m - "/media/backup/os_backup-part"
+sudo tar -czf - --exclude=backup_packages --exclude=backup.tar.gz --exclude=/bin --exclude=/home/desktop --exclude=/var/lib/docker --exclude=/media --exclude=/dev --exclude=/mnt --exclude=/proc  --exclude=/sys --exclude=/tmp / | split -b 512m - "/media/backup/os_backup-part"
 sudo cp /media/backup/* /media/discworld/backup
