348a9d12abdff556f503dfbe00af354552a0c812
Andreas <andreas_osby@hotmail.com>
2022-02-09 20:36:35 +0100

    updated script to check internet in container

--- testInternet.sh
+++ testInternet.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # check ovpn status - if no access restart vpn
-wget -q --spider https://www.ovpn.com/v2/api/client/ptr
+# wget -q --spider https://www.ovpn.com/v2/api/client/ptr
+#docker exec vpn_client /bin/bash -c "wget -q --spider https://www.ovpn.com/v2/api/client/ptr"
 
-if [ $? -ne 0 ]; then
-    echo "Offline"
-    sudo service openvpn restart
-fi
+#if [ $? -ne 0 ]; then
+#    echo "Offline"
+    # sudo service openvpn restart
+ cd ~/git/docker && docker-compose up -d vpn_client deluge jackett sonarr
+#fi
