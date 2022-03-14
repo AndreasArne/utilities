c12ba8f4562b57e5686f725fe0401973942d55e7
Andreas Arnesson <andreas_osby@hotmail.com>
2022-03-14 21:11:53 +0100

    update kill script

--- kill-after-start.bat
+++ kill-after-start.bat
@@ -1,9 +1,10 @@
 @echo
 cls
-timeout 5 >nul
+timeout 10 >nul
 taskkill /IM hid.exe /T /F
 taskkill /IM Gaming.exe /T /F
-taskkill /IM CC_Engine_x64.exe /T /F
+@REM taskkill /IM CC_Engine_x64.exe /T /F
 taskkill /IM LEDKeeper2.exe /T /F
+taskkill /IM MSIAfterburner.exe /T /F
 
 wsl --shutdown
